2fc64c4f691fce65a0869d9f0e286ad00117e759
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-21 14:45:11 -0400

    Made second and third formatted dates

--- src/main/java/com/pluralsight/FormattingDates.java
+++ src/main/java/com/pluralsight/FormattingDates.java
@@ -13,5 +13,20 @@ public class FormattingDates {
         String formattedDate = todaysDate.format(formatter);
         //Print formatted date
         System.out.println(formattedDate);
+
+        // Made a new date formatter
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        //Put the formatted date into new string variable
+        String formattedDate2 = todaysDate.format(formatter2);
+        //Print the second formatted date
+        System.out.println(formattedDate2);
+
+        //Made third date formatter
+        DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("EEEE MM, yyyy");
+        //Put the formatted date into new string variable
+        String formattedDate3 = todaysDate.format(formatter3);
+        //Print the third formatted date
+        System.out.println(formattedDate3);
+
     }
 }
