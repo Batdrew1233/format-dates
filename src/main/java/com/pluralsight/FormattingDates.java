429b1c3e051b5d7597cd6ac52bf2fdf358bd9e74
Batdrew1233 <drewstevenson1233@gmail.com>
2026-04-21 14:38:05 -0400

    printed formatted date

--- src/main/java/com/pluralsight/FormattingDates.java
+++ src/main/java/com/pluralsight/FormattingDates.java
@@ -1,4 +1,17 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class FormattingDates {
+    public static void main(String[] args) {
+        //Get current date
+        LocalDate todaysDate = LocalDate.now();
+        //Format the date
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        //Put formatted date into string
+        String formattedDate = todaysDate.format(formatter);
+        //Print formatted date
+        System.out.println(formattedDate);
+    }
 }
